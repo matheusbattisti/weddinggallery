10ca51ec7493db0acf9d39e626d589b557fcd6a7
Matheus Battisti <matheus.battisti@essentia.com.br>
2019-04-15 14:15:38 -0300

    allowed hosts

--- imagegallery/settings/common.py
+++ imagegallery/settings/common.py
@@ -25,6 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+
 
 # Application definition
 
--- imagegallery/settings/development.py
+++ imagegallery/settings/development.py
@@ -1,7 +1,5 @@
 from imagegallery.settings.common import *
 
-ALLOWED_HOSTS = ['*']
-
 SECRET_KEY = '1bdo6#r7t$jw#i#tdzok+#_xite@dbi$+v%d-%&+gwve$8$#@f'
 
 DEBUG = True
